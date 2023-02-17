94f1d42f2736c28b6bff27dc717fbfe4dce8ff35
Jayphen <software@jayphen.com>
2023-02-17 22:36:22 +0100

    Complete login redirect flow

--- src/routes/login/index.tsx
+++ src/routes/login/index.tsx
@@ -1,4 +1,5 @@
 import { component$ } from "@builder.io/qwik";
+import type { CookieOptions, RequestHandler } from "@builder.io/qwik-city";
 import { action$, Form, z, zod$ } from "@builder.io/qwik-city";
 import type { mastodon } from "masto";
 import { login } from "masto";
@@ -8,33 +9,55 @@ import { error, form } from "./style.css";
 export const appStore: Map<string, mastodon.v1.Client> = new Map();
 
 export const useLogin = action$(
-  async ({ server }, { fail }) => {
+  async ({ server }, { fail, cookie, redirect }) => {
     let masto: mastodon.Client;
+    let app: mastodon.v1.Client;
+    const cookieConfig: CookieOptions = {
+      httpOnly: true,
+      path: "/",
+      secure: true,
+      sameSite: "lax",
+    };
 
-    try {
-      masto = await login({ url: `https://${server}` });
-    } catch (_error) {
-      return fail(400, {
-        message:
-          "That doesn't seem like a valid Mastodon server. Please double-check!",
-      });
+    if (appStore.has(server)) {
+      app = appStore.get(server)!;
+      cookie.set("server", JSON.stringify(app), cookieConfig);
+    } else {
+      try {
+        masto = await login({ url: `https://${server}` });
+      } catch (_error) {
+        return fail(400, {
+          message:
+            "That doesn't seem like a valid Mastodon server. Please double-check!",
+        });
+      }
+
+      try {
+        app = await masto.v1.apps.create({
+          clientName: "esky-dev",
+          redirectUris: "http://localhost:5173/api/oauth",
+          scopes: "read write follow",
+        });
+
+        appStore.set(server, app);
+
+        cookie.set("server", JSON.stringify(app), cookieConfig);
+      } catch (_error) {
+        return fail(400, {
+          message:
+            "Could not connect to Mastodon server. Please try again later",
+        });
+      }
     }
 
-    try {
-      const app = await masto.v1.apps.create({
-        clientName: "esky-dev",
-        redirectUris: "http://localhost:5173/api/oauth",
-        scopes: "read write follow",
-      });
+    // Get redirect
+    const url = new URL(`https://${server}/oauth/authorize`);
+    url.searchParams.set("client_id", app.clientId || "");
+    url.searchParams.set("scope", "read write follow");
+    url.searchParams.set("redirect_uri", `http://localhost:5173/api/oauth`);
+    url.searchParams.set("response_type", "code");
 
-      appStore.set(server, app);
-
-      return { success: "Looks good! Redirecting you to auth…" };
-    } catch (_error) {
-      return fail(400, {
-        message: "Could not connect to Mastodon server. Please try again later",
-      });
-    }
+    redirect(302, url.toString());
   },
   zod$({
     server: z.string().refine(isFQDN, (val) => ({
@@ -44,46 +67,48 @@ export const useLogin = action$(
   })
 );
 
+export const onGet: RequestHandler = async ({ cookie, redirect }) => {
+  if (cookie.get("server")) {
+    redirect(302, "https://google.com");
+  }
+};
+
 export default component$(() => {
   const action = useLogin();
 
   return (
     <>
-      {action.value?.success ? (
-        <div>{action.value.success}</div>
-      ) : (
-        <Form action={action} class={form}>
-          <label for="server">At which server to you own an account?</label>
-          <div>
-            <input
-              type="text"
-              autoComplete="off"
-              name="server"
-              id="server"
-              required
-            />
-            <button type="submit">Submit</button>
+      <Form action={action} class={form}>
+        <label for="server">At which server to you own an account?</label>
+        <div>
+          <input
+            type="text"
+            autoComplete="off"
+            name="server"
+            id="server"
+            required
+          />
+          <button type="submit">Submit</button>
+        </div>
+        {action.value?.failed && (
+          <div class={error} role="alert">
+            <p>It looks like there are some errors:</p>
+            <ul>
+              <li>{action.value.message}</li>
+            </ul>
           </div>
-          {action.value?.failed && (
-            <div class={error} role="alert">
-              <p>It looks like there are some errors:</p>
-              <ul>
-                <li>{action.value.message}</li>
-              </ul>
-            </div>
-          )}
-          {action.value?.fieldErrors?.server && (
-            <div class={error} role="alert">
-              <p>It looks like there are some errors:</p>
-              <ul>
-                {action.value.fieldErrors.server.map((err) => (
-                  <li>{err}</li>
-                ))}
-              </ul>
-            </div>
-          )}
-        </Form>
-      )}
+        )}
+        {action.value?.fieldErrors?.server && (
+          <div class={error} role="alert">
+            <p>It looks like there are some errors:</p>
+            <ul>
+              {action.value.fieldErrors.server.map((err) => (
+                <li>{err}</li>
+              ))}
+            </ul>
+          </div>
+        )}
+      </Form>
     </>
   );
 });
