0f43054dfc4f7645bb7ec85dab03e90014e098fe
Jayphen <software@jayphen.com>
2023-02-18 18:14:49 +0100

    Add dotenv

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,11 @@ import { qwikVite } from "@builder.io/qwik/optimizer";
 import { qwikCity } from "@builder.io/qwik-city/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import { vanillaExtractPlugin } from "styled-vanilla-extract/vite";
+import * as dotenv from 'dotenv';
+
+dotenv.config({
+  path: process.env.NODE_ENV === 'production' ? '.env.production' : '.env'
+})
 
 export default defineConfig(() => {
   return {
