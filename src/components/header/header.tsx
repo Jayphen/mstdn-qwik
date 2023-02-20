2af24a5c0e3414ddbb0ebbd33d9aeb42f24d5538
Jayphen <software@jayphen.com>
2023-02-20 23:17:13 +0100

    Add 'logged in as'

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,33 +1,38 @@
 import { component$, useStylesScoped$ } from "@builder.io/qwik";
 import { useLocation } from "@builder.io/qwik-city";
+import { useUserDetail } from "~/routes/layout";
 import styles from "./header.css?inline";
 
 export default component$(() => {
   useStylesScoped$(styles);
+  const user = useUserDetail();
   const loc = useLocation();
 
   return (
-    <header>
-      <span>
-        You're viewing the {loc.params.instance}{" "}
-        {loc.pathname.includes("local") ? "local" : "public"} feed.
-      </span>
-      <ul>
-        <li>
-          <a
-            href={`/${loc.params.instance}/public`}
-            title={`Posts from ${loc.params.instance}, and all the other instances it knows about`}
-          >
-            Federated feed
-          </a>
-          <a
-            href={`/${loc.params.instance}/local`}
-            title={`Posts only from ${loc.params.instance}`}
-          >
-            Local feed
-          </a>
-        </li>
-      </ul>
-    </header>
+    <>
+      {user.value && <div class="loggedIn">Logged in at {user.value.url}</div>}
+      <header>
+        <span>
+          You're viewing the {loc.params.instance}{" "}
+          {loc.pathname.includes("local") ? "local" : "public"} feed.
+        </span>
+        <ul>
+          <li>
+            <a
+              href={`/${loc.params.instance}/public`}
+              title={`Posts from ${loc.params.instance}, and all the other instances it knows about`}
+            >
+              Federated feed
+            </a>
+            <a
+              href={`/${loc.params.instance}/local`}
+              title={`Posts only from ${loc.params.instance}`}
+            >
+              Local feed
+            </a>
+          </li>
+        </ul>
+      </header>
+    </>
   );
 });
